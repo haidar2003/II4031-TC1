340ae2fbf4e73bc4c0bc704750f16d911d80c0b4
Raditya Azka Prabaswara <18221152@mahasiswa.itb.ac.id>
2024-02-25 14:22:52 +0700

    extended vignere file

--- cypher/vignere_extended.py
+++ cypher/vignere_extended.py
@@ -1,16 +1,38 @@
-def vignere_extended_encrypt(plainText:str, key:str):
+def vignere_extended_encrypt(plainBytes:bytes, key:str):
     keyLength = len(key)
+    plainText = list(plainBytes)
     plainTextLength = len(plainText)
     result = ["" for i in range(plainTextLength)]
     for i in range(plainTextLength):
-        result[i] = chr((ord(plainText[i]) + ord(key[(i % keyLength)])) % 255)
-    return "".join(result)
+        result[i] = (((plainText[i]) + ord(key[i % keyLength])) % 256)
+    return bytes(result)
 
-def vignere_extended_decrypt(encryptedText:str, key:str):
+def vignere_extended_decrypt(encryptedBytes:bytes, key:str):
     keyLength = len(key)
+    encryptedText = list(encryptedBytes)
     encryptedTextLength = len(encryptedText)
     result = ["" for i in range(encryptedTextLength)]
     for i in range(encryptedTextLength):
-        result[i] = chr((ord(encryptedText[i]) - ord(key[i % keyLength])) % 255)
-    return "".join(result)
+        result[i] = ((encryptedText[i] - ord(key[i % keyLength])) % 256)
+
+    return bytes(result)
+
+def file_encrypt(plainFileName:str, encryptedFileName:str ,key:str):
+    plainFile = open(plainFileName, "rb")
+    plainBinary = plainFile.read()
+    plainFile.close()
+    encryptedBinary = vignere_extended_encrypt(plainBinary, key)
+    encryptedFile = open(encryptedFileName, "wb")
+    encryptedFile.write(encryptedBinary)
+    encryptedFile.close()
+    return list(plainBinary)
+def file_decrypt(encryptedFileName:str, plainFileName:str ,key:str):
+    encryptedFile = open(encryptedFileName, "rb")
+    encryptedBinary = encryptedFile.read()
+    encryptedFile.close()
+    plainBinary = vignere_extended_decrypt(encryptedBinary, key)
+    plainFile = open(plainFileName, "wb")
+    plainFile.write(plainBinary)
+    plainFile.close()
+    return list(plainBinary)
 
